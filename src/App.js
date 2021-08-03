227a2d1cd7ef3e17a9a4f8a5f14d7ab55501a415
Lana Salah Shamaileh <shamaylehlana62@gmail.com>
2021-08-03 23:39:03 +0300

    new

--- src/App.js
+++ src/App.js
@@ -2,17 +2,47 @@ import React from 'react'
 import Header from './components/Header.js';
 import Footer from './components/Footer.js';
 import Main from './components/Main.js';
+import Beastdata from './components/data.json';
+import SelectedBeast from './components/SelectedBeast.js';
 
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-      <Header/>
-      <Main/>
-      <Footer/>
-       </div>
-    )
+class App extends React.Component{
+  constructor(props){
+    super(props)
+    this.state={
+      Beastdata : Beastdata,
+      show: false,
+      selected: {}, 
+    }
   }
+  ShowModal=(title) =>{
+    const selectedBeast=Beastdata.find(horned => 
+      {
+        if (horned.title===title)
+        {
+          return horned; 
+        }
+        console.log(Beastdata);
+      })
+
+      this.setState({
+        selected :selectedBeast ,
+        show:true
+      }) 
+    } 
+
+   CloseModal = () => {
+    this.setState({ show: false });
+   }
+    render(){
+    return(
+      <div>
+      <Header/> 
+      <Main ShowModal={this.ShowModal} Beasts={this.state.Beastdata}/>
+      <SelectedBeast ShowModal={this.ShowModal} show={this.state.show} CloseModal={this.CloseModal} Selected={this.state.selected}/>
+      <Footer/>
+    </div>
+      )
+}
 }
 export default App;
---
+++ src/components/SelectedBeast.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import Modal from 'react-bootstrap/Modal'
+import Button from 'react-bootstrap/Button'
+import Card from 'react-bootstrap/Card'
+
+export class SelectedBeast extends Component {
+    
+    handleclose=()=> {
+        this.props.CloseModal()
+      }
+    render() {
+
+        return (
+            <div>
+                <Modal size="lg" ria-labelledby="contained-modal-title-vcenter" centered show={this.props.show} width='400px' height='400px'
+      onHide={this.handleclose}>
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          {this.props.Selected.title}
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+           <Card.Img variant="top" src={this.props.Selected.image_url} alt={this.props.Selected.keyword} width='400px' height='400px'/>
+           <Card.Title>{this.props.Selected.title}</Card.Title>
+            <Card.Text>
+              {this.props.Selected.description}
+            </Card.Text>
+            <Card.Text>
+              {this.props.Selected.keyword}
+            </Card.Text>
+            <Card.Text>
+              {this.props.Selected.horns}
+            </Card.Text>
+        </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={this.handleclose} >Close</Button>
+      </Modal.Footer>
+    </Modal> 
+     
+            </div>
+        )
+    }
+}
+
+export default SelectedBeast;
