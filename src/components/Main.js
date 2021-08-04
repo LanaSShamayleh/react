ec1ac3d08080f93cf31c591d3943b1351ce786f8
Lana Salah Shamaileh <shamaylehlana62@gmail.com>
2021-08-04 21:24:59 +0300

    new

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,28 +1,68 @@
 import React from 'react';
-import HornedBeast from './HornedBeast';
+import Horned from './HornedBeast';
 import CardGroup from 'react-bootstrap/CardGroup'
+import Form from 'react-bootstrap/Form'
+import Beastdata from './data.json';
 
 
 class Main extends React.Component{
-    render(){
-        return(
-            
-            <CardGroup>
-                  
+  selectHorns=(event)=>{
+
+    let numHorns= event.target.value
+    let allHorns=Beastdata
+    let newHorns
+    if (numHorns){
+      // eslint-disable-next-line array-callback-return
+      newHorns=allHorns.filter((item)=>{
+        // eslint-disable-next-line eqeqeq
+        if(item.horns==numHorns){
+        return item; }
+        })
+    }
+    else {
+     newHorns=allHorns;  
+    }
+    this.props.update(newHorns);
+
+}
+
+
+render(){
+ return(
+
+  <div>
+      <Form>
+          <Form.Group controlId="exampleForm.ControlSelect1">
+            <Form.Label>Number of horns : </Form.Label>
+            <Form.Control as="select" custom name='HornsNum' onChange={this.selectHorns} style = {{width: '10rem'}} centerd>
+                        <option value='1'>One Horn</option>
+                        <option value='2'>Two Horns</option>
+                        <option value='3' >Three Horns</option>
+                        <option value='100'> One Hundered Horns</option>
+                        <option value=''>All Available Horns</option>
+            </Form.Control>
+          </Form.Group>
+        </Form>
+        <CardGroup>
                  {this.props.Beasts.map(beast => {
-                  return (<HornedBeast
+                    return (<Horned
                     imageUrl={beast.image_url}
                     title={beast.title}
                     description={beast.description}
                     keyword={beast.keyword}
                     ShowModal={this.props.ShowModal}
-                    horns={beast.horns}/>
+                    horns={beast.horns}
+                    />
+                   
                     )
                  })
-               }  
-                  </CardGroup>
-           
-        )
-    }
+                 }
+              </CardGroup>
+         </div>
+            
+     
+
+      )
+}
 }
 export default Main;
